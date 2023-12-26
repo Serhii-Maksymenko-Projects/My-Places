573f4f30b61be28c9943db6c0c5214896d441c63
Serhii <maksimenko.serg1990@gmail.com>
2023-12-26 16:18:52 +0100

    Initial Commit

---
+++ My Places/My_PlacesApp.swift
@@ -0,0 +1,17 @@
+//
+//  My_PlacesApp.swift
+//  My Places
+//
+//  Created by    Sergey on 26.12.2023.
+//
+
+import SwiftUI
+
+@main
+struct My_PlacesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
